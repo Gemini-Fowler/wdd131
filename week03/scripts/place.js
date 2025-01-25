0c53a9e58b790230fc8b31ef7f9526b893122c7e
Jonas <cro24029@byui.edu>
2025-01-24 20:44:24 -0800

    housekeeping

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,23 +1,19 @@
-// Footer: Display the current year and last modified date
+
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
 
-// Wind Chill Calculation
 function calculateWindChill(temp, windSpeed) {
-  // Wind chill formula for Celsius
   return (13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)).toFixed(2);
 }
 
-// Static values for temperature and wind speed
-const temp = 18; // °C
-const windSpeed = 15; // km/h
+const temp = 18; 
+const windSpeed = 15; 
 
 const windChillElement = document.getElementById("windChill");
 
-// Check conditions for wind chill calculation
 if (temp <= 10 && windSpeed > 4.8) {
   windChillElement.textContent = `${calculateWindChill(temp, windSpeed)} °C`;
 } else {
