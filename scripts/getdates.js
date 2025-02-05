428bd949a61e686affe70f034aa13ace2fc50f03
Jonas <cro24029@byui.edu>
2025-02-04 19:12:48 -0800

    hsjt

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,7 @@
 
+
 const currentYear = new Date().getFullYear();
 
 document.querySelector('footer p:first-of-type').textContent = `© ${currentYear} All rights reserved.`;
 
-document.querySelector('footer p:nth-of-type(2)').textContent = `Note this ⚙️ CodePen summary of using the Date object in different ways. Last modified: ${document.lastModified}`;
+document.querySelector('footer p:nth-of-type(2)').textContent = `Last modified: ${document.lastModified}`;
