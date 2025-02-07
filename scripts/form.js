912dda30f34661bad9156138629e74f1db8aac9d
Jonas <cro24029@byui.edu>
2025-02-07 11:22:35 -0800

    ------.-

--- scripts/form.js
+++ scripts/form.js
@@ -1,32 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const products = [
-        {
-            id: "fc-1888",
-            name: "flux capacitor",
-            averagerating: 4.5
-        },
-        {
-            id: "fc-2050",
-            name: "power laces",
-            averagerating: 4.7
-        },
-        {
-            id: "fs-1987",
-            name: "time circuits",
-            averagerating: 3.5
-        },
-        {
-            id: "ac-2000",
-            name: "low voltage reactor",
-            averagerating: 3.9
-        },
-        {
-            id: "jj-1969",
-            name: "warp equalizer",
-            averagerating: 5.0
-        }
-    ];
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
 
+document.addEventListener('DOMContentLoaded', function() {
     const productSelect = document.getElementById("product");
 
     products.forEach(product => {
@@ -35,4 +35,23 @@ document.addEventListener("DOMContentLoaded", function () {
         option.textContent = product.name;
         productSelect.appendChild(option);
     });
+
+    let reviewCount = localStorage.getItem('reviewCount');
+    
+    if (reviewCount === null) {
+        reviewCount = 0;
+    } else {
+        reviewCount = parseInt(reviewCount, 10);
+    }
+
+    document.getElementById('review-count').textContent = `Number of reviews completed: ${reviewCount}`;
+    
+    document.getElementById('review-form').addEventListener('submit', function(event) {
+        reviewCount++;
+        
+        localStorage.setItem('reviewCount', reviewCount);
+
+        document.getElementById('review-count').textContent = `Number of reviews completed: ${reviewCount}`;
+    });
 });
+
